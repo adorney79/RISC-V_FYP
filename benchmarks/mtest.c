a32b67f2702f2d3c6a6e4fb15a5508c4053b43be
Aaron Dorney <aaronjondorney@gmail.com>
2022-01-24 10:03:15 +0000

    adding mest benchmark

---
+++ benchmarks/mtest.c
@@ -0,0 +1,118 @@
+#include "rocc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "encoding.h"
+#include <time.h>
+#define MSIZE 64
+#define PRINTSTATS 0
+static inline unsigned long read(int idx)
+{
+	unsigned long value;
+	ROCC_INSTRUCTION_DSS(0, value, 0, idx, 5);
+	return value;
+}
+
+static inline void set_x_dim(int l, int w)
+{
+
+        asm volatile ("fence");
+        ROCC_INSTRUCTION_SS(0,l, w, 0);
+}
+
+static inline void set_y_dim(int l, int w)
+{
+
+        asm volatile ("fence");
+        ROCC_INSTRUCTION_SS(0,l, w, 1);
+}
+
+static inline void set_addr(void *x_addr, void *y_addr)
+{
+
+        asm volatile ("fence");
+        ROCC_INSTRUCTION_SS(0,(uintptr_t) x_addr,(uintptr_t) y_addr, 2);
+}
+static inline void set_out(void *ptr,void *copy)
+{
+
+        asm volatile ("fence");
+        ROCC_INSTRUCTION_SS(0,(uintptr_t) ptr, (uintptr_t) copy, 3);
+}
+
+
+
+void accel();
+void bmark();
+void printm(unsigned long input[MSIZE][MSIZE]);
+unsigned long X[MSIZE][MSIZE];
+unsigned long Y[MSIZE][MSIZE];
+unsigned long Z1[MSIZE][MSIZE];
+unsigned long COPY[MSIZE][MSIZE];
+unsigned long Z2[MSIZE][MSIZE];
+
+void accel(){
+	 unsigned long result;
+        unsigned long start,end;
+        start=rdcycle();
+        set_x_dim(MSIZE,MSIZE);
+        set_y_dim(MSIZE,MSIZE);
+        set_addr(&X[0][0],&Y[0][0]);
+        set_out(&Z1[0][0],&COPY[0][0]);
+        result = read(0);
+        end=rdcycle();
+        printf("results is %lu \n",result);
+#if PRINTSTATS
+       printm(Z1);
+#endif
+        printf("rocc execution took %lu cycles \n",end-start);
+
+}
+void bmark(){
+       unsigned long result;
+        unsigned long start,end;
+        start=rdcycle();
+        for(int i=0;i<MSIZE;i++){
+		for(int j=0;j<MSIZE;j++){
+			for(int k=0;k<MSIZE;k++){
+				Z2[i][j]+=X[i][k]*Y[k][j];
+			}
+		}
+	}
+	end=rdcycle();
+#if PRINTSTATS
+       	printm(Z2);
+#endif	
+        printf("bmark execution took %lu cycles \n",end-start);
+
+}
+void printm(unsigned long input[MSIZE][MSIZE]){
+	for(int i=0;i<MSIZE;i++){
+                for(int j=0;j<MSIZE;j++){
+                        printf("%lu\t",input[i][j]);
+                        }
+                printf("\n");
+                }
+
+
+}
+int main()
+{
+	srand(time(0));
+	for(int i=0;i<MSIZE;i++){
+                for(int j=0;j<MSIZE;j++){
+			X[i][j]=rand()%1000;
+			Y[i][j]=rand()%1000;
+                        }
+                }
+#if PRINTSTATS
+        printm(X);
+	printf("multiplied by \n");
+        printm(Y);
+	printf("gives:\n");
+        #endif
+
+	accel();
+	//bmark();
+
+	return 0;
+}
